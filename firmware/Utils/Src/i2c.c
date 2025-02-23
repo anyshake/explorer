84c4942213bce36aa23711aa86005730d55275d6
Joshua Lee <24319042+bclswl0827@users.noreply.github.com>
2025-02-23 19:08:38 +0800

    fix: Disable lowpass filter to depress noise

--- firmware/Utils/Src/i2c.c
+++ firmware/Utils/Src/i2c.c
@@ -5,8 +5,6 @@ void mcu_utils_i2c_init(bool is_rtos) {
         MX_I2C1_Init();
         HAL_GPIO_WritePin(GPIOB, 6, GPIO_PIN_SET);
         HAL_GPIO_WritePin(GPIOB, 7, GPIO_PIN_SET);
-        SET_BIT(hi2c1.Instance->CR1, I2C_CR1_SWRST);
-        CLEAR_BIT(hi2c1.Instance->CR1, I2C_CR1_SWRST);
     }
     mcu_utils_delay_ms(100, is_rtos);
 }
@@ -26,12 +24,7 @@ void mcu_utils_i2c_read(uint8_t address,
         return;
     }
 
-    HAL_I2C_StateTypeDef ret = HAL_I2C_Mem_Read(&hi2c1, address << 1, reg, reg_width, rx_data, rx_len, 1000);
-    if (ret != 0) {
-        HAL_I2C_DeInit(&hi2c1);
-        MX_I2C1_Init();
-        return mcu_utils_i2c_read(address, reg, rx_data, rx_len, reg_width);
-    }
+    HAL_I2C_Mem_Read(&hi2c1, address << 1, reg, reg_width, rx_data, rx_len, 1000);
 }
 
 void mcu_utils_i2c_write(uint8_t address,
@@ -43,10 +36,5 @@ void mcu_utils_i2c_write(uint8_t address,
         return;
     }
 
-    HAL_I2C_StateTypeDef ret = HAL_I2C_Mem_Write(&hi2c1, address << 1, reg, reg_width, tx_data, tx_len, 1000);
-    if (ret != 0) {
-        HAL_I2C_DeInit(&hi2c1);
-        MX_I2C1_Init();
-        return mcu_utils_i2c_write(address, reg, tx_data, tx_len, reg_width);
-    }
+    HAL_I2C_Mem_Write(&hi2c1, address << 1, reg, reg_width, tx_data, tx_len, 1000);
 }
