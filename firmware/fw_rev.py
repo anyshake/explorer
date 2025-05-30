6aa0758df21ec0a99c8b72c438e2832692773f5f
Joshua Lee <24319042+bclswl0827@users.noreply.github.com>
2025-05-30 22:53:38 +0800

    fix: Fixed issue where firmware build fails on Windows

--- firmware/fw_rev.py
+++ firmware/fw_rev.py
@@ -5,7 +5,8 @@ def main():
     try:
         file_path = path.join(path.dirname(__file__), "..", ".git", "logs", "HEAD")
         with open(file_path, "r", encoding="utf-8") as file:
-            commit = f"git-{file.read().strip().split("\n")[-1].split(" ")[1][:7]}"
+            commit_hash = file.read().strip().split("\n")[-1].split(" ")[1][:7]
+            commit = f"git-{commit_hash}"
             print(f"'-DFW_REV=\"{commit}\"'")
     except:
         fallback = "custombuild"
